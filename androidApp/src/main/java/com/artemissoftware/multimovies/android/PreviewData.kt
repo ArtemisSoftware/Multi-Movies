7a119f18e84d9f8b0ca7829a1abc13e6ba69600e
gustavo <gustavo.isaac@gmail.com>
2024-07-01 16:40:16 +0100

    PreviewData

---
+++ androidApp/src/main/java/com/artemissoftware/multimovies/android/PreviewData.kt
@@ -0,0 +1,19 @@
+package com.artemissoftware.multimovies.android
+
+import com.artemissoftware.multimovies.android.home.HomeState
+import com.artemissoftware.multimovies.domain.models.Movie
+
+internal object PreviewData {
+
+    val movie = Movie(
+        id = 1,
+        title = "Movie 1",
+        description = "The first movie",
+        imageUrl = "Https://www.movie1.jpg",
+        releaseDate = "01-01-1990"
+    )
+
+    val homeState = HomeState(
+        movies = listOf(movie)
+    )
+}
