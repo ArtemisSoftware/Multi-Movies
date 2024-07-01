ec655b8399b496e9a33300da99dfcdca806a6866
gustavo <gustavo.isaac@gmail.com>
2024-07-01 12:02:47 +0100

    GetMoviesUseCase

---
+++ shared/src/commonMain/kotlin/com/artemissoftware/multimovies/domain/usecase/GetMoviesUseCase.kt
@@ -0,0 +1,15 @@
+package com.artemissoftware.multimovies.domain.usecase
+
+import com.artemissoftware.multimovies.domain.models.Movie
+import com.artemissoftware.multimovies.domain.repository.MovieRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class GetMoviesUseCase: KoinComponent {
+    private val repository: MovieRepository by inject()
+
+    @Throws(Exception::class)
+    suspend operator fun invoke(page: Int): List<Movie>{
+        return repository.getMovies(page = page)
+    }
+}
