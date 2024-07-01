464c57b4d66dc0afbce1b2415d1a496fd951b728
gustavo <gustavo.isaac@gmail.com>
2024-07-01 12:28:09 +0100

    RepositoryModule

---
+++ shared/src/commonMain/kotlin/com/artemissoftware/multimovies/data/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.multimovies.data.di
+
+import com.artemissoftware.multimovies.data.repository.MovieRepositoryImpl
+import com.artemissoftware.multimovies.domain.repository.MovieRepository
+import org.koin.dsl.module
+
+private val repositoryModule = module {
+    single<MovieRepository> { MovieRepositoryImpl(get()) }
+}
