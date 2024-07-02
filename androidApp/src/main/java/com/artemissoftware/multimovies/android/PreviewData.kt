f259b468b1fb2df465ffe093bf4f48847c506c46
gustavo <gustavo.isaac@gmail.com>
2024-07-02 05:42:01 +0100

    DetailScreen

--- androidApp/src/main/java/com/artemissoftware/multimovies/android/PreviewData.kt
+++ androidApp/src/main/java/com/artemissoftware/multimovies/android/PreviewData.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.multimovies.android
 
+import com.artemissoftware.multimovies.android.detail.DetailState
 import com.artemissoftware.multimovies.android.home.HomeState
 import com.artemissoftware.multimovies.domain.models.Movie
 
@@ -15,5 +16,9 @@ internal object PreviewData {
 
     val homeState = HomeState(
         movies = listOf(movie)
+    )
+
+    val detailState = DetailState(
+        movie = movie
     )
 }
---
+++ androidApp/src/main/java/com/artemissoftware/multimovies/android/detail/DetailScreen.kt
@@ -0,0 +1,126 @@
+package com.artemissoftware.multimovies.android.detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.artemissoftware.multimovies.android.MyApplicationTheme
+import com.artemissoftware.multimovies.android.PreviewData
+import com.artemissoftware.multimovies.android.R
+import com.artemissoftware.multimovies.android.theme.Red
+
+@Composable
+fun DetailScreen(
+    state: DetailState
+) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        state.movie?.let { movie ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.background)
+            ) {
+                AsyncImage(
+                    model = movie.imageUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(320.dp)
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(20.dp)
+                ) {
+                    Text(
+                        text = movie.title,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.fillMaxWidth().height(46.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Red
+                        ),
+                        elevation = ButtonDefaults.buttonElevation(
+                            defaultElevation = 0.dp
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_play_button),
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(text = "Start watching now", color = Color.White)
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Released in ${movie.releaseDate}".uppercase(),
+                        style = MaterialTheme.typography.bodySmall
+                    )
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    Text(text = movie.description, style = MaterialTheme.typography.bodySmall)
+                }
+            }
+        }
+
+        if (state.loading){
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Red
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DetailScreenPreview() {
+    MyApplicationTheme {
+        DetailScreen(
+            state = PreviewData.detailState,
+        )
+    }
+}
