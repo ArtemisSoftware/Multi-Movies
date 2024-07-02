b4e2eec90d2fd3136a2e24f4553c42c22d00287e
gustavo <gustavo.isaac@gmail.com>
2024-07-02 11:54:08 +0100

    Log

--- shared/src/commonMain/kotlin/com/artemissoftware/multimovies/data/network/MovieApi.kt
+++ shared/src/commonMain/kotlin/com/artemissoftware/multimovies/data/network/MovieApi.kt
@@ -1,6 +1,8 @@
 package com.artemissoftware.multimovies.data.network
 
 
+import io.github.aakira.napier.Napier
+import io.github.aakira.napier.log
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.HttpRequestBuilder
@@ -9,10 +11,23 @@ import io.ktor.http.path
 import io.ktor.http.takeFrom
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 
 internal abstract class MovieApi {
 
     val client = HttpClient {
+
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Napier.d(tag = "TAG_KTOR_LOGGER", message = message )
+                }
+            }
+            level = LogLevel.ALL
+        }
+
         install(ContentNegotiation){
             json(Json {
                 ignoreUnknownKeys = true
